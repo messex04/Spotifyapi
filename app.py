cc2a7408997cd834bed9b77951d1b6a38bc2fe90
messex04 messex04 <messina.daniele.2004@gmail.com>
2025-02-10 11:48:34 +0000

    finito pt2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "f726b08700854dd8997d401bdb42894b"
 SPOTIFY_CLIENT_SECRET = "7fb5909e43b244099dfed6689cf4ef9f"
-SPOTIFY_REDIRECT_URI = "https://5000-messex04-spotifyapi-09skfv6lipe.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-messex04-spotifyapi-nzrwv4809zh.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -17,6 +17,14 @@ sp_oauth = SpotifyOAuth(
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
     scope="user-read-private"
+)
+#config SpotifyOAuth per l'autenticazione e redirect uri
+sp_oauth = SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 @app.route('/')
 def login():
@@ -36,7 +44,16 @@ def home():
         return redirect(url_for('login'))
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+    return render_template('home.html', user_info=user_info, playlists=playlists_info) #passo le info utente all'home.html
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
